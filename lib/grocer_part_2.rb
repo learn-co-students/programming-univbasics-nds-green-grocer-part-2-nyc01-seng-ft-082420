2789dccb3a033de606a6e160daadc05277e6696c
Ian Rosen <irosen419@gmail.com>
2020-04-30 17:15:26 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,23 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  couponed_array = []
+  couponed_item = {}
+  coupons.each do |coupon|
+    cart.map do |grocery_item|
+      if grocery_item[:item] == coupon[:item]
+        couponed_item[:item] = "#{grocery_item[:item]} W/ COUPON"
+        couponed_item[:price] = coupon[:cost] / coupon[:num]
+        couponed_item[:clearance] = grocery_item[:clearance]
+        couponed_item[:count] = grocery_item[:count]
+        couponed_array << grocery_item
+        couponed_array << couponed_item
+      end
+    end
+  end
+  binding.pry
+  return couponed_array
 end
 
 def apply_clearance(cart)
@@ -12,14 +26,14 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
 end
 
-def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
-end
+# def checkout(cart, coupons)
+#   # Consult README for inputs and outputs
+#   #
+#   # This method should call
+#   # * consolidate_cart
+#   # * apply_coupons
+#   # * apply_clearance
+#   #
+#   # BEFORE it begins the work of calculating the total (or else you might have
+#   # some irritated customers
+# end
