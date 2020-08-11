67c9281fb41b746df134f73c6a9caef03dbec5df
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 03:24:35 +0000

    Done.

--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -1,11 +1,7 @@
 def find_item_by_name_in_collection(name, collection)
-  index = 0
-
   collection.each do |grocery_item|
     return grocery_item if grocery_item[:item] === name 
-    index += 1
   end
-
   nil
 end
 
